65200c2b29376491da888e46c4c13b311b76f4e4
vim <v.mechytashvili@gmail.com>
2019-06-13 12:28:59 +0300

    Add HTB user object

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player(object):
+    def __init__(self, name, score, users, roots, rank, last, prev):
+        self.name = name
+        self.score = score
+        self.users = users
+        self.roots = roots
+        self.rank = rank
+        self.last = last
+        self.prev = prev
